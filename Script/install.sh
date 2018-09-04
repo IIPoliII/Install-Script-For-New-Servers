4796df3eb0c97ab0dafcd7ffe8b2bb196f2ba976
root <raimundophilippe@gmail.com>
2018-09-04 14:26:47 +0200

    Update 2018-09-04

--- Script/install.sh
+++ Script/install.sh
@@ -5,7 +5,8 @@
 Yes_No ()
 {
   # print question
-  echo -n "Do you want to install everything : "
+  echo -e "\033[33mWelcome to Poli's server instllation tool\033[0m"
+  echo -ne "\e[41mDo you want to install everything :"
 
   # read answer
   read YnAnswer
@@ -20,16 +21,16 @@ Yes_No ()
    "YES")  Start_Install ;;
      "Y")  Start_Install ;;
      "y")  Start_Install ;;
-    "NO") exit 0
-          echo "These apps will be installed one by one" ;;
-    "No") exit 0
-          echo "These apps will be installed one by one" ;;
-     "n") exit 0
-          echo "These apps will be installed one by one" ;;
-     "N") exit 0
-          echo "These apps will be installed one by one" ;;
-    "no") exit 0 
-	  echo "These apps will be installed one by one" ;;
+    "NO") echo "These apps will be installed one by one"
+	  Start_Install_One_One ;;
+    "No") echo "These apps will be installed one by one"
+	  Start_Install_One_One ;;
+     "n") echo "These apps will be installed one by one"
+	  Start_Install_One_One ;;
+     "N") echo "These apps will be installed one by one"
+	  Start_Install_One_One	;;
+    "no") echo "These apps will be installed one by one"
+	  Start_Install_One_One ;;
     *)      echo "Please answer yes or no (y|n)" ; Yes_No ;;
   esac
 }
@@ -37,7 +38,7 @@ Yes_No ()
 # function that is started when answer is yes
 Start_Install ()
 {
-	echo "Good, continuing installation"
+	echo -e "\e[41mGood, continuing installation"
   	apt update -y
 	apt upgrade -y
 	apt dist-upgrade -y
@@ -46,6 +47,106 @@ Start_Install ()
 	apt update -y
 	apt upgrade -y
 	apt dist-upgrade -y
+exit 0
+}
+Start_Install_One_One ()
+{
+	echo -e "\e[41mGood, installing one by one"
+        echo -ne "\e[41mDo you want to install the updates (Recommended) :"
+        read update
+        case $update in
+   "yes")  apt update -y  ;;
+   "Yes")  apt update -y  ;;
+   "YES")  apt update -y  ;;
+     "Y")  apt update -y  ;;
+     "y")  apt update -y  ;;
+    "NO") echo "Won't be installed" ;;
+    "No") echo "Won't be installed" ;;
+     "n") echo "Won't be installed" ;;
+     "N") echo "Won't be installed" ;;
+    "no") echo "Won't be installed" ;;
+    *)      echo "Please answer yes or no (y|n)" ; Yes_No ;;
+  esac
+        echo -ne "\e[41mDo you want to install the upgarde (Recommended) :"
+        read upgarde
+        case $upgarde in
+   "yes")  apt upgrade -y  ;;
+   "Yes")  apt upgrade -y  ;;
+   "YES")  apt upgrade -y  ;;
+     "Y")  apt upgrade -y  ;;
+     "y")  apt upgrade -y  ;;
+    "NO") echo "Won't be installed" ;;
+    "No") echo "Won't be installed" ;;
+     "n") echo "Won't be installed" ;;
+     "N") echo "Won't be installed" ;;
+    "no") echo "Won't be installed" ;;
+    *)      echo "Please answer yes or no (y|n)" ; Yes_No ;;
+  esac
+
+        echo -ne "\e[41mDo you want to clean the cache of the upgrades (Recommended) :"
+        read distupgrade
+        case $distupgrade in
+   "yes")  apt dist-upgrade -y  ;;
+   "Yes")  apt dist-upgrade -y  ;;
+   "YES")  apt dist-upgrade -y  ;;
+     "Y")  apt dist-upgrade -y  ;;
+     "y")  apt dist-upgrade -y  ;;
+    "NO") echo "Won't be installed" ;;
+    "No") echo "Won't be installed" ;;
+     "n") echo "Won't be installed" ;;
+     "N") echo "Won't be installed" ;;
+    "no") echo "Won't be installed" ;;
+    *)      echo "Please answer yes or no (y|n)" ; Yes_No ;;
+  esac
+
+	echo -ne "\e[41mDo you want to install openssh-server :"
+ 	read openssh
+	case $openssh in
+   "yes")  apt install openssh-server ;;
+   "Yes")  apt install openssh-server ;;
+   "YES")  apt install openssh-server ;;
+     "Y")  apt install openssh-server ;;
+     "y")  apt install openssh-server ;;
+    "NO") echo "Won't be installed" ;;
+    "No") echo "Won't be installed" ;;
+     "n") echo "Won't be installed" ;;
+     "N") echo "Won't be installed" ;;
+    "no") echo "Won't be installed" ;;
+    *)      echo "Please answer yes or no (y|n)" ; Yes_No ;;
+  esac
+	echo -ne "\e[41mDo you want to install curl :"
+        read curl
+        case $curl in
+   "yes")  apt install curl ;;
+   "Yes")  apt install curl ;;
+   "YES")  apt install curl ;;
+     "Y")  apt install curl ;;
+     "y")  apt install curl ;;
+    "NO") echo "Won't be installed" ;;
+    "No") echo "Won't be installed" ;;
+     "n") echo "Won't be installed" ;;
+     "N") echo "Won't be installed" ;;
+    "no") echo "Won't be installed" ;;
+    *)      echo "Please answer yes or no (y|n)" ; Yes_No ;;
+  esac
+
+        echo -ne "\e[41mDo you want to install sudo :"
+        read sudo
+        case $sudo in
+   "yes")  apt install sudo ;;
+   "Yes")  apt install sudo ;;
+   "YES")  apt install sudo ;;
+     "Y")  apt install sudo ;;
+     "y")  apt install sudo ;;
+    "NO") echo "Won't be installed" ;;
+    "No") echo "Won't be installed" ;;
+     "n") echo "Won't be installed" ;;
+     "N") echo "Won't be installed" ;;
+    "no") echo "Won't be installed" ;;
+    *)      echo "Please answer yes or no (y|n)" ; Yes_No ;;
+  esac
+
+
 exit 0
 }
 Yes_No
