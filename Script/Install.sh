9532858e95db5330542f6b98673b509e3a35ee94
Poli <raimundophilippe@gmail.com>
2018-11-16 14:49:21 +0100

    Update Install.sh

--- Script/Install.sh
+++ Script/Install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Made by Poli
 apt install screen -y
-screen "bash -c '$(wget -qO - https://raw.githubusercontent.com/IIPoliII/Install-Script-For-New-Servers/master/Script/InstallAll.sh)"'
+screen 'bash -c "$(wget -qO - https://raw.githubusercontent.com/IIPoliII/Install-Script-For-New-Servers/master/Script/InstallAll.sh)"'
