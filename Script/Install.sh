20ad5dfa34a2ad29c8717ef945bf1abdecb4bae2
Poli <raimundophilippe@gmail.com>
2018-11-16 15:02:22 +0100

    Update Install.sh

--- Script/Install.sh
+++ Script/Install.sh
@@ -4,6 +4,7 @@ apt install screen -y
 mkdir tmp
 cd tmp
 wget https://raw.githubusercontent.com/IIPoliII/Install-Script-For-New-Servers/master/Script/InstallAll.sh
+chmod +x InstallAll.sh
 screen ./InstallAll.sh
 cd ..
 rm -r tmp
