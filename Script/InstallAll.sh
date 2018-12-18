134ef539a62e9baf598d8a56488bd4ab3ac309a5
Poli <raimundophilippe@gmail.com>
2018-12-18 16:21:37 +0100

    Update InstallAll.sh

--- Script/InstallAll.sh
+++ Script/InstallAll.sh
@@ -36,7 +36,7 @@ Start_Install ()
   	apt update -y
 	apt upgrade -y
 	apt dist-upgrade -y
-	apt install -y openssh-server wget curl nano sed libssl-dev libpam0g-dev zlib1g-dev dh-autoreconf software-properties-common speedtest-cli apache2 proftpd screen php htop
+	apt install -y openssh-server wget curl nano unzip sed libssl-dev libpam0g-dev zlib1g-dev dh-autoreconf software-properties-common speedtest-cli apache2 proftpd screen php htop
 	apt install links2 shellinabox-y
 	apt update -y
 	apt upgrade -y
